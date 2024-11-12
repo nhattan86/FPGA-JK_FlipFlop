de7205b92ba4446a0b9de66a42ed7ee87d16329c
nhattan <121374626+nhattan86@users.noreply.github.com>
2024-11-13 00:37:23 +0700

    Update JK.vhd

--- JK.vhd
+++ JK.vhd
@@ -1,27 +1,32 @@
 library ieee;
 use ieee.std_logic_1164.all;
-entity JK is
-  port (J,K,SET,CLR: in std_logic;
-		  Q,Qb: out std_logic);
-end JK;
-Architecture T of JK is
-signal t: std_logic;
-  begin 
-   process (J,K,CLR,SET)
-	 begin
-	 	  if SET = '0' then
-	     t <= '1';
-	  elsif CLR = '0' then
-	        t <= '0';
-	  elsif( J = '0' and K = '0') then
-           t <= t;
-     elsif (J = '0' and K = '1') then
-           t <= '0';
-	  elsif (J = '1' and K = '0') then
-           t <= '1';
-	  elsif (J = '1' and K = '1') then
-	        t <= not t;
-	  end if;	
-	Q<=t ; 
+
+entity jk_flip_flop is
+  port (
+    j, k, set, clr : in std_logic;
+    q, q_bar : out std_logic
+  );
+end jk_flip_flop;
+
+architecture T of jk_flip_flop is
+  signal t : std_logic;
+begin
+  process (j, k, clr, set)
+  begin
+    if set = '0' then
+      t <= '1';
+    elsif clr = '0' then
+      t <= '0';
+    elsif (j = '0' and k = '0') then
+      t <= t;
+    elsif (j = '0' and k = '1') then
+      t <= '0';
+    elsif (j = '1' and k = '0') then
+      t <= '1';
+    elsif (j = '1' and k = '1') then
+      t <= not t;
+    end if;
+    q <= t;
+    q_bar <= not t;
   end process;
 end T;
